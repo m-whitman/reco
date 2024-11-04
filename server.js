ddcd3ca3e6176c879fcfcad573f74388250137b5
Michael Whitman <41151385+m-whitman@users.noreply.github.com>
2024-11-04 19:32:28 +0000

    remove mongodb

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
@@ -10,11 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
 // Import routes
 const searchRoutes = require('./searchRouter');
@@ -30,7 +24,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
